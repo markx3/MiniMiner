392daf9e310c2d387bccf2667a4ba7d51c4da73b
Marcos Felipe Eipper <marcos@eipper.com.br>
2018-05-24 18:45:35 -0300

    Dismantled API and Problem Solver into two classes. Removed private methods from tests. TODO: Check mock value to change response accordingly

--- miniminer.py
+++ miniminer.py
@@ -1,12 +1,41 @@
+"""Miniminer."""
 import json
 from hashlib import sha256
 import requests
 import math
-import time
 from constants import token
 
+
+class MiniMinerAPI():
+    """Miniminer API.
+
+    Defines get and post methods to get problem and send solution to endpoint.
+    """
+
+    base_url = 'https://hackattic.com/challenges/mini_miner/'
+    problem_url = 'problem?access_token='
+    solution_url = 'solve?access_token='
+
+    def __init__(self, token):
+        """Initialize API with provided token."""
+        self.token = token
+
+    def get(self, url):
+        """Return problem from endpoint."""
+        response = requests.get(url + self.token).json()
+        return response
+
+    def post(self, nonce, url):
+        """Send solution to endpoint and print results."""
+        payload = {'nonce': nonce}
+        response = requests.post(url + self.token + '&playground=1',
+                                 json=payload)
+        print(response.status_code, response.json())
+        return response.json()
+
+
 class MiniMiner():
-    """Solves hackattic's MiniMiner problem in playground mode!
+    """Solves hackattic's MiniMiner problem in playground mode.
 
     A JSON is received from the endpoint with three attributes:
     'block', 'nonce' and 'difficulty'. MiniMiner's goal is to
@@ -14,53 +43,42 @@ class MiniMiner():
     start with 'difficulty' 0 bits. That is, if difficulty is
     4, the hash should start with at least 4 zero bits.
     """
+
     def __init__(self, token):
-        """Initalize MiniMiner with token provided by user"""
+        """Initalize MiniMiner with token provided by user."""
         self.debug = False
         self.token = token
-        self.get_url = 'https://hackattic.com/challenges/mini_miner/problem?access_token='
-        self.post_url = 'https://hackattic.com/challenges/mini_miner/solve?access_token='
-
-    def _get(self, url):
-        """Return problem from endpoint"""
-        response = requests.get(self.get_url + self.token).json()
-        if self.debug:
-            print(response)
-            time.sleep(5)
-        return response
-
-    def _post(self, nonce, url):
-        """Send solution to endpoint and print results"""
-        payload = { 'nonce' : nonce }
-        response = requests.post(self.post_url + self.token + '&playground=1',
-                          json=payload)
-        print(response.status_code, response.json())
-        return response.json()
 
     def run(self, debug=False):
-        """MiniMiner's main function. Fetch problem, solve and send."""
+        """Miniminer main function. Fetch problem, solve and send."""
         self.debug = debug
-        response = self._get(self.get_url)
-        nonce, digest = self._get_nonce(response['block'], response['difficulty'])
+        api = MiniMinerAPI(self.token)
+        response = api.get(api.base_url + api.problem_url)
+        nonce, digest = self._get_nonce(response['block'],
+                                        response['difficulty'])
         print(nonce, digest)
-        return(self._post(nonce, self.post_url))
-
+        return(api.post(nonce, api.base_url + api.solution_url))
 
     def _get_nonce(self, block, diff):
-        """Bruteforce nonce until expected minimum zero bits is found
-        Returns nonce and digest
+        """Bruteforce nonce until expected minimum zero bits is found.
+
+        Returns nonce and digest.
         """
         nonce = -1
         expected = '0' * math.ceil(diff/4)
         while 1:
             nonce += 1
             block['nonce'] = nonce
-            block_json = json.dumps(block, sort_keys=True, separators=(',',':'))
+            block_json = json.dumps(block,
+                                    sort_keys=True,
+                                    separators=(',', ':'))
             digest = sha256(block_json.encode()).hexdigest()
             if self.debug:
-                print('diff: ' + str(diff) + '\tnonce: ' + str(nonce) + '\tdigest: ' + str(digest))
+                msg = 'diff: %s\tnonce: %s\tdigest: %s' % (diff, nonce, digest)
+                print(msg)
             if digest.startswith(expected):
                 return nonce, digest
+
 
 if __name__ == '__main__':
     mm = MiniMiner(token)
--- test_miniminer.py
+++ test_miniminer.py
@@ -1,20 +1,24 @@
+"""MiniMiner test suite."""
 import unittest
-import json
-from unittest.mock import patch, Mock
-from miniminer import MiniMiner
+from unittest.mock import patch
+from miniminer import MiniMiner, MiniMinerAPI
+
 
 class MiniMinerTest(unittest.TestCase):
-    """MiniMiner test suite.
+    """MiniMiner test class.
+
     Creates a test problem based off hackattic's example. POST and GET
     functions are patched to return expected results. Expected nonce from
     test block is 45.
     """
 
-    test_problem = {'block':{'data':[],'nonce':None}, 'difficulty':8}
+    # TODO: Check mock value to change response accordingly
+
+    test_problem = {'block': {'data': [], 'nonce': None}, 'difficulty': 8}
     passed = {'result': 'passed (playground mode)'}
 
-
     def setUp(self):
+        """Patch requests get and post methods."""
         get_patcher = patch('miniminer.requests.get')
         self.mock_get = get_patcher.start()
         self.mock_get.return_value.status_code = 200
@@ -26,26 +30,24 @@ class MiniMinerTest(unittest.TestCase):
         self.mock_post.return_value.json.return_value = self.passed
 
     def test_get(self):
-        test_problem_json = self.test_problem
-        response = MiniMiner('someToken')._get('null')
+        """Get test problem from stub and check it."""
+        response = MiniMinerAPI('someToken').get('null')
         self.assertEqual(response, self.test_problem)
 
     def test_post(self):
+        """Post solution to stub and check it."""
         passed = {'result': 'passed (playground mode)'}
-        response = MiniMiner('someToken')._post(45, 'null')
+        response = MiniMinerAPI('someToken').post(45, 'null')
         self.assertEqual(response, passed)
 
-    def test_get_nonce(self):
-        test_problem = self.test_problem
-        block = test_problem['block']
-        diff = test_problem['difficulty']
-        nonce = MiniMiner('token')._get_nonce(block, diff)
-        self.assertEqual(nonce[0], 45)
-
     def test_run(self):
+        """Run MiniMiner's main method.
+
+        Check if response is OK.
+        """
         result = MiniMiner('token').run()
         self.assertEqual(result, self.passed)
 
+
 if __name__ == '__main__':
     unittest.main()
-
